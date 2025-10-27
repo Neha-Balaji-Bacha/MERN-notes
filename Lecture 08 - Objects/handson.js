6c1c7501b65fec6b9090f0bcb4e641e297303207
Neha-Balaji-Bacha <neha170603@gmail.com>
2025-10-27 13:40:15 +0530

    Object fundamentals in JavaScript

---
+++ Lecture 08 - Objects/handson.js
@@ -0,0 +1,200 @@
+const classRoom = [
+  {
+    name: "neha",
+    age: 23,
+    gender: "female",
+    grade: "B",
+    address: {
+      addressLine1: "varje",
+      addressLine2: "baner",
+      city: "Latur",
+      homeNo: 31,
+    },
+  },
+  {
+    name: "nikita",
+    age: 24,
+    gender: "female",
+    grade: "A",
+    address: {
+      addressLine1: "aundh",
+      addressLine2: "balewadi",
+      city: "pune",
+      homeNo: 32,
+    },
+  },
+  {
+    name: "nikhil",
+    age: 25,
+    gender: "male",
+    grade: "A",
+    address: {
+      addressLine1: "katraj",
+      addressLine2: "shivaji chowk",
+      city: "udgir",
+      homeNo: 51,
+    },
+  },
+  {
+    name: "dixsha",
+    age: 27,
+    gender: "female",
+    grade: "+A",
+    address: {
+      addressLine1: "pisoli chowk",
+      addressLine2: "chandni",
+      city: "solapur",
+      homeNo: 41,
+    },
+  },
+  {
+    name: "Mayuri",
+    age: 29,
+    gender: "female",
+    grade: "a",
+    address: {
+      addressLine1: "Mahalunge",
+      addressLine2: "karve",
+      city: "Mumbai",
+      homeNo: 52,
+    },
+  },
+];
+// const classRoom = [student1, student2, student3, student4, student5];
+for (let student of classRoom) {
+  //   if (student && student.address && student.address.city == "pune") {
+  //     console.log(student.name, student.age, student.address, student.homeNo);
+  //   }
+
+  if (student?.address?.city === "pune") {
+    console.log(student.name, student.age, student.address, student.homeNo);
+  }
+}
+
+const person = {
+  name: "Neha",
+  age: 23,
+  address: {
+    city: "Pune",
+    pin: 411001,
+    geo: {
+      lat: 18.52,
+      lng: 73.85,
+    },
+  },
+};
+
+console.log(person.address.city); // Pune
+console.log(person.address.geo.lat); // 18.52
+
+//avoid errors
+console.log(person.address.geo.lng); // 73.85
+console.log(person.work?.company); // undefined (safe, no error)
+
+//Adding a property inside a nested object
+person.address.country = "India";
+console.log(person.address);
+// { city: 'Pune', pin: 411001, geo: { lat: 18.52, lng: 73.85 }, country: 'India' }
+
+//Updating nested values
+person.address.geo.lat = 19.07; // change latitude
+console.log(person.address.geo.lat); // 19.07
+
+//loop
+const obj = { name: "Neha", age: 23, city: "Pune" };
+console.log("for...of on object:");
+for (let val of obj) {
+  console.log(val);
+} //TypeError: obj is not iterable
+
+const arr1 = ["Neha", 23, "Pune"];
+console.log("for...of on array");
+for (let val of arr1) {
+  console.log(val);
+}
+// Neha
+// 23
+// Pune
+
+const studentnew = {
+  name: "Neha",
+  age: 23,
+  city: "Pune",
+};
+// 🔹 Using Object.keys()
+console.log("---- Keys ----");
+for (let key of Object.keys(studentnew)) {
+  console.log(key); // only keys
+}
+
+// 🔹 Using Object.values()
+console.log("---- Values ----");
+for (let value of Object.values(studentnew)) {
+  console.log(value); // only values
+}
+
+// 🔹 Using Object.entries()
+console.log("---- Entries ----");
+for (let [key, value] of Object.entries(studentnew)) {
+  console.log(key, value); // both key and value
+}
+
+const arr1 = ["a", "b", "c"];
+
+console.log("for...in on array:");
+for (let i in arr1) {
+  console.log(i, arr1[i]);
+}
+// for...in on array:
+// 0 a
+// 1 b
+// 2 c
+
+const obj1 = { name: "Neha", age: 23, city: "Pune" };
+console.log("for...in on object:");
+for (let key in obj1) {
+  console.log(key, obj1[key]);
+} //op
+// for...in on object:
+// name Neha
+// age 23
+// city Pune
+
+//Array inside Object
+const stu = { name: "Priya", skills: ["JS", "C++", "MERN"] };
+
+// for...in → keys of object
+for (let i in stu) {
+  console.log("for...in (obj):", stu.skills[i]);
+}
+
+// for...of → values of skills array
+for (let skill of stu.skills) {
+  console.log("for...of (skills):", skill);
+}
+
+const student = {
+  name: "Neha",
+  greet() {
+    console.log("Hello " + this.name);
+  },
+};
+
+//Objects inside Array
+const classroom = [
+  { name: "Aarav", city: "Pune" },
+  { name: "Rohan", city: "Mumbai" },
+];
+
+// for...of → objects
+for (let student of [classroom[0]]) {
+  console.log("for...of:", student.name, student.city);
+}
+
+// for...in → keys inside each object
+for (let key in classroom[0]) {
+  console.log("for...in (first obj):", key, classroom[0][key]);
+}
+
+const student1 = { name: "Piyush", address: {} };
+console.log(student1); //{ name: 'Piyush', address: {} }
---
+++ Lecture 08 - Objects/script.js
@@ -0,0 +1,225 @@
+//create variables to store name ,age,add,Class of student
+const name = "piyush";
+const age = 17;
+const address = "Pune";
+const Class = 12;
+
+const studentObj = {
+  name: "piyush",
+  age: 17,
+  address: "Pune",
+  Class: 12,
+};
+// //create variables to store name ,age,add,Class of parent
+const parentName = "Riya";
+const parentage = 35;
+const parentAddress = "pune";
+
+const parentobj = {
+  parentName: "Riya",
+  parentage: 35,
+  parentAddress: "pune",
+};
+
+//access properties in object
+console.log("student name:", studentObj.name);
+console.log("student name:", studentObj["name"]);
+const studentProperty = "name";
+console.log("student name:", studentObj[studentProperty]);
+
+//updatinga value of an object
+studentObj.name = "neha";
+console.log("student name:", studentObj.name);
+
+//add any property to an object
+parentobj.gender = "female";
+parentobj.standard = 12;
+console.log(parentobj);
+
+//delete any property
+delete parentobj.standard;
+console.log(parentobj);
+
+//create a person properties --> name,age,city
+const personObj = {
+  name: "Neha",
+  age: 23,
+  city: "Latur",
+};
+personObj.hobby = "singing"; //add
+personObj.city = "Nagpur"; //update
+console.log(personObj.age); //print age
+console.log(personObj.standard); //undefined
+//checker
+if (personObj && personObj.address)
+  //all properties on left of dot(.) must not be undefined
+  console.log(personObj.address.firstLine);
+
+//nested objects
+const employeeObj = {
+  name: "Raju",
+  company: "Acciojob",
+  salary: "10LPA",
+  address: {
+    addressLine1: "Bhumkar chowk",
+    addressLine2: "Wakad",
+    houseNo: 31,
+    city: "pune",
+  },
+};
+console.log("city of", employeeObj.name, "is", employeeObj.address.city);
+
+//arrays objects
+const studentObjArr = [
+  studentObj,
+  {
+    name: "pulkit",
+    age: 27,
+    address: "nashik",
+    standard: 10,
+  },
+];
+console.log(studentObjArr);
+
+//or
+
+const studentObj2 = {
+  name: "pulkit",
+  age: 27,
+  address: "nashik",
+  standard: 10,
+};
+const studentObjArr2 = [studentObj, studentObj2];
+
+//using loop
+for (let i in studentObj) {
+  console.log(i, ":", studentObj[i]);
+}
+
+//print all keys
+console.log("keys:", Object.keys(studentObj));
+
+//print all values
+console.log("values:", Object.values(studentObj));
+
+console.log("Using object.keys() method to get the array of keys");
+for (let i of Object.keys(studentObj)) {
+  console.log(i, ":", studentObj[i]);
+}
+console.log("values:", Object.values(studentObj));
+
+student1 = {
+  name: "neha",
+  age: 23,
+  gender: "female",
+  grade: "B",
+  address: {
+    addressLine1: "varje",
+    addressLine2: "baner",
+    city: "Latur",
+    homeNo: 31,
+  },
+};
+student2 = {
+  name: "nikita",
+  age: 24,
+  gender: "female",
+  grade: "A",
+  address: {
+    addressLine1: "aundh",
+    addressLine2: "balewadi",
+    city: "pune",
+    homeNo: 32,
+  },
+};
+student3 = {
+  name: "nikhil",
+  age: 25,
+  gender: "male",
+  grade: "A",
+  address: {
+    addressLine1: "katraj",
+    addressLine2: "shivaji chowk",
+    city: "udgir",
+    homeNo: 51,
+  },
+};
+student4 = {
+  name: "dixsha",
+  age: 27,
+  gender: "female",
+  grade: "+A",
+  address: {
+    addressLine1: "pisoli chowk",
+    addressLine2: "chandni",
+    city: "solapur",
+    homeNo: 41,
+  },
+};
+student5 = {
+  name: "Mayuri",
+  age: 29,
+  gender: "female",
+  grade: "a",
+  address: {
+    addressLine1: "Mahalunge",
+    addressLine2: "karve",
+    city: "Mumbai",
+    homeNo: 52,
+  },
+};
+const classRoom = [student1, student2, student3, student4, student5];
+for (let student of classRoom) {
+  if (student && student.address && student.address.city == "pune") {
+    console.log(student.name, student.age, student.address, student.homeNo);
+  }
+}
+//function inside object
+const carObj = {
+  car: "Mercedes GLS",
+  color: "white",
+  milega: 10,
+  currentSpeed: 0,
+  running: function running() {
+    console.log("The car is running!!");
+  },
+};
+carObj.running();
+
+//this keyword :
+const carObj1 = {
+  car: "Mercedes GLS",
+  color: "white",
+  milega: 10,
+  currentSpeed: 0,
+  running: function running() {
+    console.log("The car is running!! current speed is:", this.currentSpeed);
+  },
+  //  running: function () {   // anonymous function
+  //   console.log("The car is running!! current speed is:", this.currentSpeed);
+  // }
+  //  running() {
+  //   console.log("The car is running!! current speed is:", this.currentSpeed);
+  // }
+};
+carObj.running();
+//each obj has its own "this" keyword --> mostly used inside the object.functions can be used inside obj
+
+//task
+const portfolio = {
+  name: "piyush",
+  skills: ["c++", "Javascript", "MERN"],
+  showSkills: function showSkills() {
+    let allSkills = "";
+    for (let skill of this.skills) {
+      allSkills += skill + ",";
+    }
+    console.log(allSkills);
+  },
+  addskills: function addskills(skillName) {
+    this.skills.push(skillName);
+  },
+};
+portfolio.showSkills();
+portfolio.addskills("node.js");
+portfolio.showSkills();
