68617cc10412ab9273b8cd63de1204e93bbe705d
Neha-Balaji-Bacha <neha170603@gmail.com>
2025-11-16 12:32:25 +0530

    event propogation & forms validation

--- Lecture 37 - Forms in DOM & mini project/form.js
+++ Lecture 37 - Forms in DOM & mini project/form.js
@@ -1,23 +1,29 @@
-//H.W : submit button make disable when required field not fill
 const users = [];
 const submitbtn = document.querySelector("button");
 const form = document.querySelector("form");
 
-const inputs = document.querySelectorAll("input, select");
-submitbtn.disabled = true;
+submitbtn.addEventListener("click", (event) => {
+  event.preventDefault();
 
-inputs.forEach((input) => {
-  input.addEventListener("input", () => {
-    const name = document.querySelector("#name").value;
-    const mobile = document.querySelector("#mobile").value;
-    const gender = document.querySelector("input[name=gender]:checked")?.value;
-    const consent = document.querySelector("#consent").checked;
-    const city = document.querySelector("#city").value;
+  const user = {};
+  user.name = document.querySelector("input[type=text]").value;
+  user.number = document.querySelector("input[type=number]").value;
+  user.gender = document.querySelector("input[name=gender]:checked")?.value;
+  user.consent = document.querySelector("input[type=checkbox]")?.checked;
+  user.city = document.querySelector("#city")?.value;
 
-    if (name && mobile && gender && consent && city) {
-      submitbtn.disabled = false;
-    } else {
-      submitbtn.disabled = true;
-    }
-  });
+  if (
+    !user.name ||
+    !user.number ||
+    !user.gender ||
+    !user.consent ||
+    !user.city
+  ) {
+    alert("Please fill out all the fields");
+  } else {
+    users.push(user);
+    form.reset();
+    console.log(users);
+    submitbtn.disabled = true;
+  }
 });
--- Lecture 37 - Forms in DOM & mini project/handson.js
+++ Lecture 37 - Forms in DOM & mini project/handson.js
@@ -1,49 +1,23 @@
+//H.W : submit button make disable when required field not fill
 const users = [];
 const submitbtn = document.querySelector("button");
 const form = document.querySelector("form");
 
-submitbtn.addEventListener("click", (event) => {
-  event.preventDefault();
+const inputs = document.querySelectorAll("input, select");
+submitbtn.disabled = true;
 
-  const user = {};
-  user.name = document.querySelector("input[type=text]").value;
-  user.number = document.querySelector("input[type=number]").value;
-  user.gender = document.querySelector("input[name=gender]:checked")?.value;
-  user.consent = document.querySelector("input[type=checkbox]")?.checked;
-  user.city = document.querySelector("#city")?.value;
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    const name = document.querySelector("#name").value;
+    const mobile = document.querySelector("#mobile").value;
+    const gender = document.querySelector("input[name=gender]:checked")?.value;
+    const consent = document.querySelector("#consent").checked;
+    const city = document.querySelector("#city").value;
 
-  if (
-    !user.name ||
-    !user.number ||
-    !user.gender ||
-    !user.consent ||
-    !user.city
-  ) {
-    alert("Please fill out all the fields");
-  } else {
-    users.push(user);
-    form.reset();
-    console.log(users);
-    submitbtn.disabled = true;
-  }
+    if (name && mobile && gender && consent && city) {
+      submitbtn.disabled = false;
+    } else {
+      submitbtn.disabled = true;
+    }
+  });
 });
-
-//H.W
-// const inputs = document.querySelectorAll("input, select");
-// submitbtn.disabled = true;
-
-// inputs.forEach((input) => {
-//   input.addEventListener("input", () => {
-//     const name = document.querySelector("#name").value;
-//     const mobile = document.querySelector("#mobile").value;
-//     const gender = document.querySelector("input[name=gender]:checked")?.value;
-//     const consent = document.querySelector("#consent").checked;
-//     const city = document.querySelector("#city").value;
-
-//     if (name && mobile && gender && consent && city) {
-//       submitbtn.disabled = false;
-//     } else {
-//       submitbtn.disabled = true;
-//     }
-//   });
-// });
