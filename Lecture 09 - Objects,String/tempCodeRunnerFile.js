bc3a3a936557d2c1c97dc2da54cc51a59af2c540
Neha-Balaji-Bacha <neha170603@gmail.com>
2025-12-06 18:31:48 +0530

    Added Object and String handling concepts with practice problems

--- Lecture 09 - Objects,String/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const string = "Neha";
-const no = 67;
-const bool = true;
-const object = { name: "nikita" };
-console.log(string.__proto__);
-console.log(no.__proto__);
-console.log(bool.__proto__);
-console.log(object.__proto__);
